0aa92db98fba7f6c21df8aab19e2a76f6dbdcc96
Anurag Vaidhya <anuragvaidhya786@gmail.com>
2020-05-20 16:07:15 +1000

    Updated deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
-docker build -t anuu1989/multi-client -f ./client/Dockerfile ./client
-docker build -t anuu1989/multi-server -f ./server/Dockerfile ./server
-docker build -t anuu1989/multi-worker -f ./worker/Dockerfile ./worker
-docker push anuu1989/multi-client 
-docker push anuu1989/multi-server
-docker push anuu1989/multi-worker
+docker build -t anuu1989/multi-client:latest -t anuu1989/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t anuu1989/multi-server:latest -t anuu1989/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t anuu1989/multi-worker:latest -t anuu1989/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push anuu1989/multi-client:latest
+docker push anuu1989/multi-server:latest
+docker push anuu1989/multi-worker:latest
+
+docker push anuu1989/multi-client:$SHA
+docker push anuu1989/multi-server:$SHA
+docker push anuu1989/multi-worker:$SHA
+
 kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=anuu1989/multi-server:$SHA
+kubectl set image deployments/client-deployment client=anuu1989/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=anuu1989/multi-worker:$SHA
